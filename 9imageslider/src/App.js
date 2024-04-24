67f114f893140f1628fb8b13d211a881ce005040
SujitSolav <solavsujit@gmail.com>
2024-04-24 10:22:35 +0530

    modified:   7moviesapp 	modified:   9imageslider/package-lock.json 	modified:   9imageslider/package.json 	modified:   9imageslider/src/App.js 	new file:   9imageslider/src/Componant/Home.js 	new file:   9imageslider/src/Componant/Navbar.css 	new file:   9imageslider/src/Componant/Navbar.js 	modified:   9imageslider/src/index.js

--- 9imageslider/src/App.js
+++ 9imageslider/src/App.js
@@ -1,22 +1,14 @@
+import React from 'react';
 import './App.css';
-import Bmical from './Componant/Bmical';
-import Faq from './Componant/Faq';
-import Gitsearch from './Componant/Gitsearch';
-import Quote from './Componant/Quote';
-import Shoping from './Componant/Shoping';
-import Slider from './Componant/Slider';
-import Videoplayer from './Componant/Videoplayer';
+import {Outlet} from  'react-router-dom';
+import Navbar from './Componant/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <Slider/>
-      <Faq/>
-      <Quote/>
-      <Shoping/>
-      <Gitsearch/>
-      <Videoplayer/>
-      <Bmical/>
+      <Navbar/>
+      <Outlet/>
     </div>
   );
 }
--- 9imageslider/src/index.js
+++ 9imageslider/src/index.js
@@ -3,11 +3,62 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {  RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Bmical from './Componant/Bmical';
+import Faq from './Componant/Faq';
+import Gitsearch from './Componant/Gitsearch';
+import Quote from './Componant/Quote';
+import Shoping from './Componant/Shoping';
+import Slider from './Componant/Slider';
+import Videoplayer from './Componant/Videoplayer';
+import Home from './Componant/Home';
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:'/',
+        element: <Home/>
+      },
+      {
+        path:'/Bmicl',
+        element: <Bmical/>
+      },
+      {
+        path:'/Faq',
+        element: <Faq/>
+      },
+      {
+        path:'/Gitsearch',
+        element: <Gitsearch/>
+      },
+      {
+        path:'/Quote',
+        element: <Quote/>
+      },
+      {
+        path:'/Shoping',
+        element: <Shoping/>
+      },
+      {
+        path:'/Slider',
+        element: <Slider/>
+      },
+      {
+        path:'/Video',
+        element: <Videoplayer/>
+      },
+    ]
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
@@ -15,3 +66,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
